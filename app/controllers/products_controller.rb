28c5e3b9ad7dcf1c8b7d06853c9b518dd7f46335
“lukenilsson” <luke.nilsson@gmail.com>
2023-05-09 14:45:08 -0400

    scraping product names and prices

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,20 +2,25 @@ class ProductsController < ApplicationController
   require "nokogiri"
   require "httparty"
 
-  def index
-  end
-
   def scrape_products
     url = "https://www.chewy.com/brands/homeopet-6907?nav-submit-button=&ref-query=homeopet&ref=searchRedirect"
 
     response = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(response.body)
 
-    products = parsed_page.css(".kib-product-title")
+    product_names = parsed_page.css(".kib-product-title")
+    product_prices = parsed_page.css(".kib-product-pricing__row")
 
-    names = products.map do |product|
+    names = product_names.map do |product|
       title = product.css("div.kib-product-title__text").text.strip
-    end
-    render json: { names: names }
+      title unless title.include?("Bundle:")
+    end.compact
+
+    prices = product_prices.map do |product|
+      price = product.css("div.kib-product-price.kib-product-price--md").text.strip.gsub("Chewy Price", "").strip
+      price unless price.include?("Autoship Price") || price.include?("$29.98")
+    end.compact
+
+    render json: { names: names, prices: prices }
   end
 end
